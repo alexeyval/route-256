ec3484cd10cdf13052d26147b8572f52dd168d31
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-02-03 15:13:50 +0500

    OK

--- v-task-03-pair-programming.go
+++ v-task-03-pair-programming.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
 )
 
 func main() {
 	var t int
-	fmt.Scan(&t)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscan(reader, &t)
 	for i := 0; i < t; i++ {
 		var n int
-		fmt.Scan(&n)
+		fmt.Fscan(reader, &n)
 		a := make([]int, n)
 
 		for j := 0; j < n; j++ {
-			fmt.Scan(&a[j])
+			fmt.Fscan(reader, &a[j])
 		}
 		for j := 0; j < n; j++ {
 			if a[j] == 0 {
@@ -22,16 +25,21 @@ func main() {
 			}
 			suitable := -1
 			for k := j + 1; k < n; k++ {
+				if a[k] == 0 {
+					continue
+				}
 				if suitable == -1 {
 					suitable = k
 				}
-				if diff(a[k], a[j]) < diff(a[j], a[suitable]) {
+				if diff(a[k], a[j]) < diff(a[suitable], a[j]) {
 					suitable = k
 				}
 			}
+			a[j] = 0
 			a[suitable] = 0
 			fmt.Println(j+1, suitable+1)
 		}
+		fmt.Println()
 	}
 }
 
